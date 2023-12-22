c01493321df343ec8e7158067950ee6c4f6ce34d
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2023-12-22 09:32:45 +0100

    Frontend Refused data (Fear my return) 09:33:09 up 15:58,  1 user,  load average: 0.69, 0.79, 1.28 USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT samuel   tty2     tty2             Thu17   15:58m  0.04s  0.03s /usr/libexec/gnome-session-binary --session=ubuntu

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,23 @@ import express from "express";
 import { port, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 // Middleware to parse JSON data
 app.use(express.json());
+// Middleware to handle CORS policy
+// Optoin 1 - Allow all origins using cors default options
+// app.use(cors());
 
+// option 2 - Allow specific origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.get("/", (req, res) => {
   res.status(234).send("Hello World");
 });
